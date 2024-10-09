d137abe3c1b38610803a4b3bb3f24669fa727097
Colin Kohler <colink78@gmail.com>
2024-10-09 10:30:30 -0400

    Moving new spherical harmonics implentation to branch

--- torch_harmonics/spherical_harmonics.py
+++ torch_harmonics/spherical_harmonics.py
@@ -3,136 +3,11 @@
 import numpy as np
 import torch
 from torch import nn
-import torch.fft
 
 from lie_learn.representations.SO3 import spherical_harmonics
 from lie_learn.spaces import S2
 
 from torch_harmonics.harmonic_function import HarmonicFunction
-
-
-def lobatto_weights(n, a=-1.0, b=1.0, tol=1e-16, maxiter=100):
-    r"""
-    Helper routine which returns the Legendre-Gauss-Lobatto nodes and weights
-    on the interval [a, b]
-    """
-
-    wlg = np.zeros((n,))
-    tlg = np.zeros((n,))
-    tmp = np.zeros((n,))
-
-    # Vandermonde Matrix
-    vdm = np.zeros((n, n))
-
-    # initialize Chebyshev nodes as first guess
-    for i in range(n):
-        tlg[i] = -np.cos(np.pi * i / (n - 1))
-
-    tmp = 2.0
-
-    for i in range(maxiter):
-        tmp = tlg
-
-        vdm[:, 0] = 1.0
-        vdm[:, 1] = tlg
-
-        for k in range(2, n):
-            vdm[:, k] = (
-                (2 * k - 1) * tlg * vdm[:, k - 1] - (k - 1) * vdm[:, k - 2]
-            ) / k
-
-        tlg = tmp - (tlg * vdm[:, n - 1] - vdm[:, n - 2]) / (n * vdm[:, n - 1])
-
-        if max(abs(tlg - tmp).flatten()) < tol:
-            break
-
-    wlg = 2.0 / ((n * (n - 1)) * (vdm[:, n - 1] ** 2))
-
-    # rescale
-    tlg = (b - a) * 0.5 * tlg + (b + a) * 0.5
-    wlg = wlg * (b - a) * 0.5
-
-    return tlg, wlg
-
-
-def legpoly(mmax, lmax, x, norm="ortho", inverse=False, csphase=True):
-    r"""
-    Computes the values of (-1)^m c^l_m P^l_m(x) at the positions specified by x.
-    The resulting tensor has shape (mmax, lmax, len(x)). The Condon-Shortley Phase (-1)^m
-    can be turned off optionally.
-
-    method of computation follows
-    [1] Schaeffer, N.; Efficient spherical harmonic transforms aimed at pseudospectral numerical simulations, G3: Geochemistry, Geophysics, Geosystems.
-    [2] Rapp, R.H.; A Fortran Program for the Computation of Gravimetric Quantities from High Degree Spherical Harmonic Expansions, Ohio State University Columbus; report; 1982;
-        https://apps.dtic.mil/sti/citations/ADA123406
-    [3] Schrama, E.; Orbit integration based upon interpolated gravitational gradients
-    """
-
-    # compute the tensor P^m_n:
-    nmax = max(mmax, lmax)
-    vdm = np.zeros((nmax, nmax, len(x)), dtype=np.float64)
-
-    norm_factor = 1.0 if norm == "ortho" else np.sqrt(4 * np.pi)
-    norm_factor = 1.0 / norm_factor if inverse else norm_factor
-
-    # initial values to start the recursion
-    vdm[0, 0, :] = norm_factor / np.sqrt(4 * np.pi)
-
-    # fill the diagonal and the lower diagonal
-    for l in range(1, nmax):
-        vdm[l - 1, l, :] = np.sqrt(2 * l + 1) * x * vdm[l - 1, l - 1, :]
-        vdm[l, l, :] = (
-            np.sqrt((2 * l + 1) * (1 + x) * (1 - x) / 2 / l) * vdm[l - 1, l - 1, :]
-        )
-
-    # fill the remaining values on the upper triangle and multiply b
-    for l in range(2, nmax):
-        for m in range(0, l - 1):
-            vdm[m, l, :] = (
-                x
-                * np.sqrt((2 * l - 1) / (l - m) * (2 * l + 1) / (l + m))
-                * vdm[m, l - 1, :]
-                - np.sqrt(
-                    (l + m - 1)
-                    / (l - m)
-                    * (2 * l + 1)
-                    / (2 * l - 3)
-                    * (l - m - 1)
-                    / (l + m)
-                )
-                * vdm[m, l - 2, :]
-            )
-
-    if norm == "schmidt":
-        for l in range(0, nmax):
-            if inverse:
-                vdm[:, l, :] = vdm[:, l, :] * np.sqrt(2 * l + 1)
-            else:
-                vdm[:, l, :] = vdm[:, l, :] / np.sqrt(2 * l + 1)
-
-    vdm = vdm[:mmax, :lmax]
-
-    if csphase:
-        for m in range(1, mmax, 2):
-            vdm[m] *= -1
-
-    return vdm
-
-
-def _precompute_legpoly(mmax, lmax, t, norm="ortho", inverse=False, csphase=True):
-    r"""
-    Computes the values of (-1)^m c^l_m P^l_m(\cos \theta) at the positions specified by t (theta).
-    The resulting tensor has shape (mmax, lmax, len(x)). The Condon-Shortley Phase (-1)^m
-    can be turned off optionally.
-
-    method of computation follows
-    [1] Schaeffer, N.; Efficient spherical harmonic transforms aimed at pseudospectral numerical simulations, G3: Geochemistry, Geophysics, Geosystems.
-    [2] Rapp, R.H.; A Fortran Program for the Computation of Gravimetric Quantities from High Degree Spherical Harmonic Expansions, Ohio State University Columbus; report; 1982;
-        https://apps.dtic.mil/sti/citations/ADA123406
-    [3] Schrama, E.; Orbit integration based upon interpolated gravitational gradients
-    """
-
-    return legpoly(mmax, lmax, np.cos(t), norm=norm, inverse=inverse, csphase=csphase)
 
 
 class SphericalHarmonics(HarmonicFunction):
@@ -147,32 +22,72 @@ class SphericalHarmonics(HarmonicFunction):
        num_lon - Number of elements on the longitudinal axis.
     """
 
-    def __init__(self, L: int, M: int, num_lat: int = 360, num_lon: int = 360):
+    def __init__(
+        self, L: int, grid_type="lie_learn", num_theta: int = 360, num_phi: int = 360
+    ):
         super().__init__()
 
-        self.num_lat = num_lat
-        self.num_lon = num_lon
-        self.mmax = M
-        self.lmax = L
+        self.L = L
+        self.grid_type = grid_type
+        self.num_theta = num_theta
+        self.num_phi = num_phi
 
-        cost, _ = lobatto_weights(self.num_lat, -1, 1)
-        t = np.flip(np.arccos(cost))
-        pct = _precompute_legpoly(self.mmax, self.lmax, t)
-        pct = torch.from_numpy(pct)
+        Y = self.generate_basis_fns()
+        self.register_buffer("Y", Y, persistent=False)
 
-        self.register_buffer("pct", pct, persistent=False)
+    def generate_basis_fns(self, coords: torch.Tensor = None) -> torch.Tensor:
+        if coords is None:
+            if self.grid_type == "lie_learn":
+                self.grid = S2.meshgrid(self.num_theta, grid_type="Driscoll-Healy")
+                self.num_theta = self.grid[0].shape[0]
+                self.num_phi = self.grid[0].shape[1]
+
+                theta, phi = self.grid
+            else:
+                self.grid = np.meshgrid(
+                    np.linspace(0, np.pi, self.num_theta),
+                    np.linspace(0, 2 * np.pi, self.num_phi),
+                )
+
+                theta, phi = self.grid
+        else:
+            theta = coords[:, 0].view(-1, 1)
+            phi = coords[:, 1].view(-1, 1)
+
+        irreps = np.arange(self.L + 1)
+        ls = [[ls] * (2 * ls + 1) for ls in irreps]
+        ls = np.array(
+            [ll for sublist in ls for ll in sublist]
+        )  # 0, 1, 1, 1, 2, 2, 2, 2, 2, ...
+
+        ms = [list(range(-ls, ls + 1)) for ls in irreps]
+        ms = np.array(
+            [mm for sublist in ms for mm in sublist]
+        )  # 0, -1, 0, 1, -2, -1, 0, 1, 2, ...
+
+        Y = spherical_harmonics.sh(
+            ls[:, None, None],
+            ms[:, None, None],
+            theta[None, :, :],
+            phi[None, :, :],
+            field="real",
+            normalization="quantum",
+            condon_shortley=True,
+        )
+
+        return torch.tensor(Y).float()
 
     def forward(self, w: torch.Tensor, coords: torch.Tensor = None) -> torch.Tensor:
-        B = w.size(0)
+        B, R, _ = w.shape
 
-        # w = torch.view_as_real(w)
+        if coords is not None:
+            Y = self.generate_basis_fns(coords.cpu())
+            Y = Y.permute(1, 0, 2).unsqueeze(3).unsqueeze(1)
+            Y = Y.to(w.device)
+        else:
+            Y = self.Y.view(1, 1, *self.Y.shape)
+            Y = Y.expand(B, R, Y.size(2), Y.size(3), Y.size(4))
 
-        rl = torch.einsum("...lm, mlk->...km", w[..., 0], self.pct.to(w.dtype))
-        im = torch.einsum("...lm, mlk->...km", w[..., 1], self.pct.to(w.dtype))
-        xs = torch.stack((rl, im), -1)
-
-        # apply the inverse (real) FFT
-        x = torch.view_as_complex(xs)
-        out = torch.fft.irfft(x, n=self.num_lon, dim=-1, norm="forward")
+        out = torch.einsum("brn,brncd->brncd", w, Y).sum(2)
 
         return out
